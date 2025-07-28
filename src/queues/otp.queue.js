25ec8e1f44bb89c9eefc631163474fa9f3f071b1
AashishRautela <asisrautela612@gmail.com>
2025-07-29 00:59:56 +0530

    feat: remove debug logs from sendOtpJob and add epic and issue model files

--- src/queues/otp.queue.js
+++ src/queues/otp.queue.js
@@ -7,9 +7,5 @@ export const otpQueue = new Queue('otpQueue', {
 
 // Function to add job
 export const sendOtpJob = async (data) => {
-  console.log('data at send otp job', data);
-
-  console.log('called otp->');
   await otpQueue.add('sendOtp', data);
-  console.log('job added->');
 };
